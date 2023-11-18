41d396ef51e11ce85fec95a0f5210177ea62ff99
Alexander Maso <amaso@mo.ece.pdx.edu>
2023-11-17 23:33:53 -0800

    Blast off

--- DCandPT/syn/rtl/four_bit_select_adder.sv
+++ DCandPT/syn/rtl/four_bit_select_adder.sv
@@ -99,3 +99,43 @@ module four_bit_select_adder (
     assign output_Cout = Cout_reg;
 
 endmodule
+
+module two_bit_adder (
+    input logic 	[1:0] 		a, b,  
+    input logic 			Cin, 
+    output logic 	[1:0] 		sum, 
+    output logic 			Cout);
+
+    // Internal carry signal
+    logic carry;
+
+    // First 1bit adder
+    full_adder fa0 (
+        .a(a[0]),
+        .b(b[0]),
+        .Cin(Cin),
+        .sum(sum[0]),
+        .Cout(carry)
+    );
+
+    // Second 1bit adder
+    full_adder fa1 (
+        .a(a[1]),
+        .b(b[1]),
+        .Cin(carry),
+        .sum(sum[1]),
+        .Cout(Cout)
+    );
+
+endmodule
+
+// Full adder module for a single bit
+module full_adder (
+    input logic 		a, b, Cin,
+    output logic 		sum, Cout);
+
+    assign sum= a ^ b ^ Cin;
+    assign Cout = (a & b) | (a & Cin) | (b & Cin);
+
+endmodule
+
